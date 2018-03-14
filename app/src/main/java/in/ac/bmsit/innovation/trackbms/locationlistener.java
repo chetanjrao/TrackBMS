c1a22d5336834253d0e3c0e3a967a6cc1b22a0e6
chethanjkulkarni <chethanjkulkarni@gmail.com>
2018-03-14 19:26:54 +0530

    Initial commit

---
+++ app/src/main/java/in/ac/bmsit/innovation/trackbms/locationlistener.java
@@ -0,0 +1,32 @@
+package in.ac.bmsit.innovation.trackbms;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+/**
+ * Created by Chethan on 3/14/2018.
+ */
+
+public class locationlistener implements LocationListener {
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+}
